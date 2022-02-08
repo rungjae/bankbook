65351cde2a2902e272b92ec2f387092b2d510349
rungjae <sorrylee0907@nate.com>
2022-02-08 20:36:22 +0900

    주석추가 출금 조건문 수정필요 / 모듈화 필요

--- src/main/java/studyproject/pj1/bankbook/mapper/BankbookMapper.java
+++ src/main/java/studyproject/pj1/bankbook/mapper/BankbookMapper.java
@@ -7,16 +7,27 @@ import java.util.List;
 
 @Mapper
 public interface BankbookMapper {
+    /**
+     * BANKBOOK 테이블 한 행의 모든 정보 반환
+     */
     @Select("SELECT * FROM BANKBOOK")
     List<BankbookDTO> findUseAll();
-
+    /**
+     * SEQ 값에 해당하는 행의 모든 정보 반환
+     */
     @Select("SELECT * FROM BANKBOOK WHERE SEQ = #{seq}")
     BankbookDTO findUse(Long seq);
-
+    /**
+     * BANKBOOK 의 내역 정보들을 추가
+     * useGeneratedKeys = true 자동생성된 키값을 가져온다
+     * keyProperty = "seq" seq 필드로 키값을 받는다
+     */
     @Insert("INSERT INTO BANKBOOK(use, daw, price, balance, paymentTime) VALUES (#{use}, #{daw}, #{price}, #{balance}, now())")
     @Options(useGeneratedKeys = true, keyProperty = "seq")
     int addUse(BankbookDTO dto);
-    //현재 잔액 업데이트용
+    /**
+     * SEQ 값에 해당하는 행의 잔액을 반환
+     */
     @Update("UPDATE BANKBOOK SET balance = #{balance} WHERE seq = #{seq}")
     int upBalance(int balance, Long seq);
 }
